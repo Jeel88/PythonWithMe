7e827613610f72e95eed564be1754250cad98303
Jeel <jeelsavaliya888@gmail.com>
2026-08-09 15:30:17 +0530

    operators in py

---
+++ _06_operators.py
@@ -0,0 +1,26 @@
+a=20
+b=10
+#arithmetic operator
+print("Addition:", a+b)
+print("Subtraction:", a-b)
+print("Multiplication:", a*b)
+print("Division:", a/b)
+print("Floor Division:", a//b) 
+print("Modulus:", a%b)
+print("Power:", a**b) 
+
+#comparision operator
+print("Equal:", a==b)
+print("Not Equal:", a!=b)
+print("Greater Than:", a>b)
+print("Less Than:", a<b)
+print("Greater or Equal:", a>=b)
+print("Less or Equal:", a<=b)
+
+#logical operator
+x=True
+y=False
+
+print("AND:", x and y)
+print("OR:", x or y)
+print("NOT:", not x)

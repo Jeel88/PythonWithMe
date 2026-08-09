ecb6323937c2deb9c8373e5addcbcdb62ab3c7ce
Jeel <jeelsavaliya888@gmail.com>
2026-08-09 15:08:44 +0530

    VAriables in py

---
+++ Basic Code/_02_variable.py
@@ -0,0 +1,16 @@
+name="jeel" #string
+print(name)
+print(type(name))
+
+
+age=20 #int
+print(age)
+print(type(age))
+
+fav_num=8.88 #float
+print(fav_num)
+print(type(fav_num))
+
+is_student=True #Boolean
+print(is_student)
+print(type(is_student))

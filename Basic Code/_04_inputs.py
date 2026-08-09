3f93c1014b38c01b8570d7b3bcc4c76bd3044f54
Jeel <jeelsavaliya888@gmail.com>
2026-08-09 15:22:49 +0530

    Inputs in py other type and Type Conversion

--- Basic Code/_04_inputs.py
+++ Basic Code/_04_inputs.py
@@ -4,3 +4,5 @@ age = input("Enter Ur Age : ")
 print("Hello, ",name)
 print("You Are ",age," years old")
 
+print(f"Heeloo {name} ") # another option
+
---
+++ Basic Code/_05_TypeConversion.py
@@ -0,0 +1,33 @@
+age = "20"
+height = "5.6"
+
+print(age)
+print(type(age))
+
+print(height)
+print(type(height))
+
+#converting
+age = int(age)
+height = float(height)
+
+#after conversion
+print(age)
+print(type(age))
+
+print(height)
+print(type(height))
+
+#int to string
+number = 100
+text = str(number)
+
+print(text)
+print(type(text))
+
+#number to bool
+value = 1
+result = bool(value)
+
+print(result)
+print(type(result))

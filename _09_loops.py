f9fd09fb340d7b9cb34c73bccb2eb51b51b7735a
Jeel <jeelsavaliya888@gmail.com>
2026-08-09 16:05:47 +0530

    loops in py

---
+++ _09_loops.py
@@ -0,0 +1,17 @@
+for i in range (5):
+    print(i)
+for i in range(1,6):
+    print(i)  
+for i in range(1,20,2):
+    print(i)
+#with string
+name="python"
+for i in name:
+    print(i)
+
+total=0
+for i in range(1,6):
+    print(i)
+    total+=i
+print("Total: ",total)
+        

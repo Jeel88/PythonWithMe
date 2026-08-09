7c4c232a0cd3bcf6acc838b095dbde7a1e45a166
Jeel <jeelsavaliya888@gmail.com>
2026-08-09 15:11:48 +0530

    Datatype in py

---
+++ Basic Code/_03_datatype.py
@@ -0,0 +1,17 @@
+name = "Jeel" #string
+age = 20 #integer
+fav_number = 5.8 #Float
+is_student = True #boolean
+result = None #noneType
+
+print(name)
+print(age)
+print(fav_number)
+print(is_student)
+print(result)
+
+print(type(name))
+print(type(age))
+print(type(fav_number))
+print(type(is_student))
+print(type(result))

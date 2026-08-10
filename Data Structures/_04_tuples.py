6d91fad314daaedb4b63f577b1ce4a417347820a
Jeel <jeelsavaliya888@gmail.com>
2026-08-10 15:08:58 +0530

    TUples & T Methods in py

---
+++ Data Structures/_04_tuples.py
@@ -0,0 +1,14 @@
+fruits=("apple","banana","cherry","strawberry")
+print(fruits)
+
+print(fruits[0])
+print(fruits[-1])
+
+print(fruits[0:3])
+print(fruits[::-1])
+
+print(len(fruits))
+
+numbers = (10, 20, 10, 30, 10)
+print(numbers.count(10))
+print(numbers.index(20))

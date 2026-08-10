0aba50b0f5a49b7b2a19ae9fe031657033ee11cf
Jeel <jeelsavaliya888@gmail.com>
2026-08-10 14:44:24 +0530

    Lists Indexing Slicing in py

---
+++ Data Structures/_02_ListIndexingSlicing.py
@@ -0,0 +1,15 @@
+numbers=[10,20,30,40,50]
+print(numbers)
+print(numbers[0])
+print(numbers[2])
+print(numbers[-1])
+#slicing
+print(numbers[0:4])
+print(numbers[:3])
+print(numbers[:])
+print(numbers[::2])#gap of 2
+print(numbers[::-1])#rev oder
+
+
+
+

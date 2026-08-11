e657d475e2b5a6b94609ce0bca22ad5dc10fd28f
Jeel <jeelsavaliya888@gmail.com>
2026-08-11 17:16:49 +0530

    Key words Arguments

---
+++ Functions/_05_keywordArguments.py
@@ -0,0 +1,7 @@
+def introduce(name, age, course):
+    print(f"Name: {name}")
+    print(f"Age: {age}")
+    print(f"Course: {course}")
+introduce("jeel",20,"Btech IT")    
+#this is key word args
+introduce(age=20,course="Btech IT",name="heet")    

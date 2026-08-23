576dc9e9faf985a01e4c1226cc88c25b7e732e9e
Jeel <jeelsavaliya888@gmail.com>
2026-08-23 16:36:33 +0530

    Polymorphism in  py

---
+++ OOP/_06_polymorphism.py
@@ -0,0 +1,17 @@
+class Dog:
+
+    def sound(self):
+        print("Dog says: Bhaww")
+
+
+class Cat:
+
+    def sound(self):
+        print("Cat says: Meow")
+
+
+dog = Dog()
+cat = Cat()
+
+dog.sound()
+cat.sound()

10e23eaf6a237f6255d14327494e0eb8fb638922
Jeel <jeelsavaliya888@gmail.com>
2026-08-23 16:42:26 +0530

    Dunder Function in  py

---
+++ OOP/_09_dunderFunction.py
@@ -0,0 +1,12 @@
+class Team:
+
+    def __init__(self, members):
+        self.members = members
+
+    def __len__(self):
+        return len(self.members)
+
+
+team = Team(["Jeel", "Aman", "Rahul"])
+
+print(len(team))

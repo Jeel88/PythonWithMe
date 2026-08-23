67568d008b8fdcc9059cee691862cfad4a868854
Jeel <jeelsavaliya888@gmail.com>
2026-08-23 16:25:19 +0530

    constructor

---
+++ OOP/_02_constructor.py
@@ -0,0 +1,11 @@
+class Student:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+student1 = Student("Jeel", 20)
+
+print(student1.name)
+print(student1.age)
